59eb1774dbe2634f3d7fcceeb16a523cc39e2096
Silobrlst <bonesandmagic@yandex.ru>
2017-04-04 15:34:54 +0500

    1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,8 @@
+import java.util.GregorianCalendar;
+
 public class main {
     public static void main(String[] args){
-        
+        GregorianCalendar calendar = new GregorianCalendar();
+        System.err.println(calendar.getTime().toString());
     }
 }
